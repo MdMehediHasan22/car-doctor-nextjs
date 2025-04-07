5faa478318664100e30e673ae2eb7d2756b56c22
Mehedi Hasan <mdmehedi3799@gmail.com>
2025-04-07 15:02:02 +0600

    responsive fetching data

--- src/app/components/ServicesSection.jsx
+++ src/app/components/ServicesSection.jsx
@@ -164,7 +164,9 @@ export default async function ServicesSection(){
     
     return <div className="grid grid-cols-12">{data.map((item)=>{
       return (
-        <div className="col-span-4">
+        <div 
+        className="col-span-12 md:col-span-6 lg:col-span-4" 
+        key={item._id}>
             <Image src={item.img} width={314} height={208} />
         </div>
       )

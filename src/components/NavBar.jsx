4d64904ab94b57984fe1877444f8f9ee1a554638
Mehedi Hasan <mdmehedi3799@gmail.com>
2025-04-07 13:10:41 +0600

    set up the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,29 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export default function NavBar(){
+    const navMenu = ()=>{
+        return (
+            <>
+             <li>
+                <Link href={"/"}>Home</Link>
+             </li>
+             <li>
+                <Link href={"/about"}>About</Link>
+             </li>
+             <li>
+                <Link href={"/services"}>Services</Link>
+             </li>
+             <li>
+                <Link href={"/blogs"}>Blogs</Link>
+             </li>
+             <li>
+                <Link href={"/contacts"}>Contacts</Link>
+             </li>
+            </>
+        )
+    }
     return(
         <div>
            <div className="navbar bg-base-100 shadow-sm">
@@ -13,38 +35,20 @@ export default function NavBar(){
       <ul
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+       {navMenu()}
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">
-        <Image src = {'/assets/logo.svg'} width={50} height={50} />
+        <Image src = {'/assets/logo.svg'} width={80} height={80} />
     </a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {navMenu()}
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Button</a>
+    <a className="btn btn-outline">Appointment</a>
   </div>
 </div>
         </div>
